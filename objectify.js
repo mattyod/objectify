666dbabcaffc184d3d370d50b26ad44a7339ef8d
Keith Cirkel <github@keithcirkel.co.uk>
2013-08-08 15:48:12 +0100

    Simplify!

--- objectify.js
+++ objectify.js
@@ -1,53 +1,14 @@
-var _ = require('underscore');
+module.exports = function (path, value, extendObject) {
+    var ob = (extendObject = extendObject || {});
+    var splitPath = path.match(/([^\[\]]+)/g);
 
-module.exports = function (path, value) {
-    var arr, key, val, obj;
-
-    var isArrayIndex = function (item) {
-        return !isNaN(parseInt(item), 10);
-    };
-
-    var objType = function (item, parent) {
-        if (!isNaN(parseInt(item), 10)) {
-            return [];
+    splitPath.forEach(function (el, i) {
+        if (splitPath.length === i + 1) {
+            ob[el] = value;
+        } else {
+            var nextEl = splitPath[i + 1];
+            ob = (ob[el] = ob[el] || (Number.isNaN(Number(nextEl)) ? {} : []));
         }
-        if (!_.isArray(parent)) {
-            return {};
-        }
-        return false;
-    };
-
-    obj = {};
-
-    arr = path.split(/[\[\]]+/)
-        .filter(function (val) {
-            return val;
-        });
-
-    key = arr.pop();
-
-    if (isArrayIndex(key)) {
-        arr.push(key);
-        key = value;
-        value = false;
-    }
-
-    arr.reduce(function (prev, val, index, array) {
-        var o = {};
-
-        prev[val] = objType(array[index + 1], prev) || objType(array[index + 2], prev);
-
-        if (!prev[val]) {
-            if (value) {
-                o[key] = value;
-                prev[val] = o;
-            } else {
-                prev[val] = key;
-            }
-        }
-
-        return prev[val];
-    }, obj);
-
-    return obj;
+    });
+    return extendObject;
 };
--- tests/empty.js
+++ tests/empty.js
@@ -1,49 +1,7 @@
-var objectify = require('../objectify');
+var objectify = require('../objectify'),
     _ = require('underscore');
 
-var value,
-    model;
-
-// Deep extend from: https://gist.github.com/kurtmilam/1868955
-var deepExtend = function(obj) {
-    var parentRE = /#{\s*?_\s*?}/,
-        slice = Array.prototype.slice,
-        hasOwnProperty = Object.prototype.hasOwnProperty;
-
-    _.each(slice.call(arguments, 1), function(source) {
-        for (var prop in source) {
-            if (hasOwnProperty.call(source, prop)) {
-                if (_.isUndefined(obj[prop]) || _.isFunction(obj[prop]) || _.isNull(source[prop])) {
-                    obj[prop] = source[prop];
-                }
-                else if (_.isString(source[prop]) && parentRE.test(source[prop])) {
-                    if (_.isString(obj[prop])) {
-                        obj[prop] = source[prop].replace(parentRE, obj[prop]);
-                    }
-                }
-                else if (_.isArray(obj[prop]) || _.isArray(source[prop])){
-                    if (!_.isArray(obj[prop]) || !_.isArray(source[prop])){
-                        throw 'Error: Trying to combine an array with a non-array (' + prop + ')';
-                    } else {
-                        obj[prop] = _.reject(_.deepExtend(obj[prop], source[prop]), function (item) { return _.isNull(item);});
-                    }
-                }
-                else if (_.isObject(obj[prop]) || _.isObject(source[prop])){
-                    if (!_.isObject(obj[prop]) || !_.isObject(source[prop])){
-                        throw 'Error: Trying to combine an object with a non-object (' + prop + ')';
-                    } else {
-                        obj[prop] = _.deepExtend(obj[prop], source[prop]);
-                    }
-                } else {
-                    obj[prop] = source[prop];
-                }
-            }
-        }
-    });
-    return obj;
-};
-
-_.mixin({deepExtend: deepExtend});
+var value, model;
 
 module.exports = {
 
@@ -126,7 +84,7 @@ module.exports = {
             }
         };
 
-        _.deepExtend(model, objectify('days[monday][0][time]', value));
+        objectify('days[monday][0][time]', value, model);
 
         test.deepEqual(model, { days: { monday: [ { op: 'ON', time: 'foobar' } ] } });
 
@@ -141,7 +99,7 @@ module.exports = {
             }
         };
 
-        _.deepExtend(model, objectify('days[monday][0][op]', value));
+        objectify('days[monday][0][op]', value, model);
 
         test.deepEqual(model, { days: { monday: [ { op: 'foobar' } ] } });
 
@@ -156,7 +114,7 @@ module.exports = {
             }
         };
 
-        //_.deepExtend(model, objectify('days[tuesday][0][time]', value));
+        objectify('days[tuesday][0][time]', value, model);
 
         test.deepEqual(model, { days: {
             monday: [ { op: 'ON' } ],
